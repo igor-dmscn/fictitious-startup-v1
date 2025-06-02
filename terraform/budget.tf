c4b135a998a6475c2de4160934a997cac816b14e
Igor Damasceno <igoor.bruuno@gmail.com>
2025-06-01 23:18:54 -0300

    update aws budget terraform end period

--- terraform/budget.tf
+++ terraform/budget.tf
@@ -10,6 +10,6 @@ resource "aws_budgets_budget" "cost" {
   limit_unit   = "USD"
 
   time_period_start = "2025-01-01_00:00"
-  time_period_end   = "2099-12-31_23:59"
+  time_period_end   = "2027-12-31_23:59"
   time_unit         = "MONTHLY"
 }
