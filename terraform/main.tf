7fd8403f0e59f764d658c37257be85f83fc51b50
Igor Damasceno <igoor.bruuno@gmail.com>
2025-06-02 23:00:44 -0300

    fix aws provider region and private subnet cidr block

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,12 +7,12 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
 module "vpc" {
   source = "./vpc"
   vpc_cidr = "10.0.0.0/16"
   public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnet_cidrs = ["172.36.1.0/24", "172.36.2.0/24"]
+  private_subnet_cidrs = ["172.16.1.0/24", "172.16.2.0/24"]
 }
