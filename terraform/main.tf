00d68eacfe21bfa7a7ece014ce7faee60013f7bc
Igor Damasceno <igoor.bruuno@gmail.com>
2025-06-02 23:02:19 -0300

    update private subnet cidr block

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,5 +14,5 @@ module "vpc" {
   source = "./vpc"
   vpc_cidr = "10.0.0.0/16"
   public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnet_cidrs = ["172.16.1.0/24", "172.16.2.0/24"]
+  private_subnet_cidrs = ["10.0.101.0/24", "10.0.102.0/24"]
 }
