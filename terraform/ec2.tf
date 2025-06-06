da5648bdc31607af4a63c4c6782ccf4eb8742cb1
Igor Damasceno <igoor.bruuno@gmail.com>
2025-06-05 22:16:00 -0300

    fix aws_instance iam_instance_profile

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -7,13 +7,17 @@ data "aws_ami" "ubuntu" {
   }
 }
 
+data "aws_iam_instance_profile" "app_session_manager" {
+  name = "AppSessionManager"
+}
+
 resource "aws_instance" "ec2" {
   ami = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   subnet_id = module.vpc.public_subnet_ids[0]
   associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.security_group.id]
-  iam_instance_profile = "arn:aws:iam::462753922172:instance-profile/AppSessionManager"
+  iam_instance_profile = data.aws_iam_instance_profile.app_session_manager.role_name
 }
 
 resource "aws_security_group" "security_group" {
